2ab0e0e6dd71acb40eabbfe5fb5e795103a5d125
Patryk Owczarz <33702899+PatrykOwczarz@users.noreply.github.com>
2023-07-11 12:58:40 +0100

    Adding content to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,36 +22,40 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <h2>About</h2>
+
+            <h1>Hi! &#128075; I'm Patryk Owczarz</h1>
+            <h2>
+                Graduate Game Programmer based in London. <br /> <br />
+                Actively searching for Junior roles.
+            </h2>
             <div className='infoBlocks'>
                 <div className='infoBox'>
                     <h1>Games programmer</h1>
                     <p>
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua.
-                        Velit egestas dui id ornare arcu odio ut sem nulla.
+                        Computer Science with Games Technology Graduate with close to a decade
+                        of Game development experience with various engines. Including Unity, a Cherno
+                        project derived C++ engine using OpenGL and most recently Unreal. I use git as version control
+                        for all my projects. I have experience working with a
+                        multidisciplinary team of artists, game designers and UX designers.
                     </p>
                 </div>
                 <div className='infoBox'>
                     <h1>Gamer</h1>
                     <p>
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua. 21 17
-                        Velit egestas dui id ornare arcu odio ut sem nulla.
+                        I have played games since I was 5 years old.
+                        My first game ever was Neverwinter Nights which sparked my love
+                        for RPGs and later D&D. During University I played CS:GO at an amateur pro level,
+                        which taught me effective communication and teamwork. To this day I raid in a
+                        Mythic top 1000 world ranking guild in World of Warcraft.
                     </p>
                 </div>
                 <div className='infoBox'>
                     <h1>Music Enthusiast</h1>
                     <p>
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua.
-                        Velit egestas dui id ornare arcu odio ut sem nulla.
+                        My favourite music genres are metal, rock and EDM. However I can find enjoyment
+                        in most music genres. I know how to play the drums and my hidden party trick is
+                        playing guitar hero on expert difficulty. My favourite artists are Alter Bridge,
+                        Polyphia and most recently Fred again..
                     </p>
                 </div>
             </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -153,6 +153,56 @@ function Projects() {
                     </div>
 
                 </div>
+
+                <div className='projectBox'>
+                    <img src={ProfilePic} alt="Profile" width="200" height="200" />
+                    <div className='projectInfo'>
+                        <h2>Project Title</h2>
+                        <div className='projectDescription'>
+                            <p className='descriptionP'>
+                                Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua.
+                                Ut consequat semper viverra nam libero justo laoreet.
+                            </p>
+                            <div className='technologies'>
+                                <p>Technologies used:</p>
+                                <ul>
+                                    <li>
+                                        Unity
+                                    </li>
+                                    <li>
+                                        C#
+                                    </li>
+                                    <li>
+                                        Github
+                                    </li>
+                                </ul>
+                                <div className='buttons'>
+                                    <Button
+                                        variant="contained"
+                                        color="default"
+                                        href="https://github.com/PatrykOwczarz"
+                                    >
+                                        <img src={GithubIcon} alt="Github Icon" width="30" height="30"></img>
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="default"
+                                        href="./"
+                                    >
+                                        Info
+                                    </Button>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
